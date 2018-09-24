1e6bb82596f7cfb88ffd216c8fc948df725723b0
Duncan Dam <duncandam@duncans-mbp.fsm.northwestern.edu>
2018-09-24 11:44:44 -0500

    Update html  and JS

---
+++ assests/javascript/game.js
@@ -0,0 +1,126 @@
+// create variable for random number 
+var randomNumber;
+// grab element from html document
+$('#randomNumber').html(randomNumber);
+
+// create  variable for random point for each crystal
+// grab element from html document
+var redCrystalPoint;
+$('#redCrystal').html(redCrystalPoint);
+var blueCrystalPoint;
+$('#blueCrystal').html(blueCrystalPoint);
+var yellowCrystalPoint;
+$('#yellowCrystal').html(yellowCrystalPoint);
+var grennCrystalPoint;
+$('greenCrystal').html(grennCrystalPoint);
+
+// create variable to count win, losses and total score from user
+var wins = 0;
+var losses = 0;
+var totalScore;
+// grab elment from html document for wins, looses and total score
+$('#Wins').html(wins);
+$('#Losses').html(losses);
+$('#userScore').html(totalScore);
+
+//create function to generate random number 
+var randomNumber = Math.floor(Math.random() * 120 + 18) // generating numbers between 18 and 138
+
+//create function to generate random points for each crystal between 1 and 10
+var redCrystalPoint = Math.floor(Math.random() * 9 + 1);
+var blueCrystalPoint = Math.floor(Math.random() * 9 + 1);
+var yellowCrystalPoint = Math.floor(Math.random() * 9 + 1);
+var greenCrystalPoint = Math.floor(Math.random() * 9 + 1);
+
+//create function startUp function for the game
+function startUp() {
+    randomNumber;
+    console.log(randomNumber);
+    $('#randomNumber').html(randomNumber);
+    redCrystalPoint;
+    blueCrystalPoint;
+    yellowCrystalPoint;
+    greenCrystalPoint;
+    totalScore = 0;
+    $('#userScore').html(totalScore);
+}
+
+//create function to tally wins
+function win() {
+    alert("You won");
+    wins++;
+    console.log(wins);
+    $('#Wins').html(wins);
+    startUp();
+}
+
+//create function to tally losses
+function lost() {
+    alert("You lost");
+    losses++;
+    console.log(losses);
+    $('#Wins').html(wins);
+    startUp();
+}
+
+//create onlick function for redcrytsal
+$('#redCrystal').on('click', function () {
+    // add redCrystal point to totalScore
+    totalScore = totalScore + redCrystalPoint;
+    console.log(totalScore)
+    $('#userScore').html(totalScore);
+    // decide if totalScore is matched or not (win or lose)
+    if (totalScore === randomNumber) {
+        win();
+    }
+    else if (totalScore > randomNumber) {
+        lost();
+    }
+})
+//create onclick function for bluecrystal
+$('#blueCrystal').on('click', function () {
+    // add blueCrystal point to totalScore
+    totalScore = totalScore + blueCrystalPoint;
+    console.log(totalScore);
+    $('#userScore').html(totalScore);
+    // decide if totalScore is matched or not (win or lose)
+    if (totalScore === randomNumber) {
+        win();
+    }
+    else if (totalScore > randomNumber) {
+        lost();
+    }
+})
+
+//create onclick function for yellowcrystal
+$('#yellowCrystal').on('click', function () {
+    // add yellowCrystal point to totalScore
+    totalScore = totalScore + yellowCrystalPoint;
+    console.log(totalScore);
+    $('#userScore').html(totalScore);
+    // decide if totalScore is matched or not (win or lose)
+    if (totalScore === randomNumber) {
+        win();
+    }
+    else if (totalScore > randomNumber) {
+        lost();
+    }
+})
+
+//create onclick function for greencrystal
+$('#greenCrystal').on('click', function () {
+    // add greenCrystal point to totalScore
+    totalScore = totalScore + greenCrystalPoint;
+    console.log(totalScore);
+    $('#userScore').html(totalScore);
+    // decide if totalScore is matched or not (win or lose)
+    if (totalScore === randomNumber) {
+        win();
+    }
+    else if (totalScore > randomNumber) {
+        lost();
+    }
+})
+
+
+
